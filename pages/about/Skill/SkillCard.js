fb19cf8faf4852076a72c9aa9952d954eaaacef6
Billy Wu <billy31103@gmail.com>
2023-08-05 17:39:51 +0800

    add a protective logic in SkillCard.js

--- pages/about/Skill/SkillCard.js
+++ pages/about/Skill/SkillCard.js
@@ -7,6 +7,7 @@ const SkillCard = (props) => {
                 <h5 className={styles.title}>{props.title}</h5>
                 <div className={styles.skillTagContainer}>
                     {
+                        props.skills &&
                         props.skills.map((item, index) => {
                             return (
                                 <p key={index} className={styles.skillTag}>{item}</p>
